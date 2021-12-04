4b495b403a0a8b29e5cbf4adad64673c07c160b7
Bennett Smrdel <smrdelb@gmail.com>
2021-12-04 09:21:14 -0600

    set up index.js file with redux and a feedback reducer

--- src/index.js
+++ src/index.js
@@ -3,6 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App/App';
 import registerServiceWorker from './registerServiceWorker';
+import logger from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Redux
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+// Redux reducers
+const feedbackReducer = (state = [], action) => {
+    return state;
+}
+
+
+// Redux store
+const storeInstance = createStore(
+    combineReducers({
+        feedbackReducer,
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
