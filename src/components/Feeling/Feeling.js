969474f43087b92bbee7a5bcfb973c1ab1207f7f
Bennett Smrdel <smrdelb@gmail.com>
2021-12-04 09:28:12 -0600

    added feedback reducer and dispatch to Feeling component

--- src/components/Feeling/Feeling.js
+++ src/components/Feeling/Feeling.js
@@ -1,5 +1,14 @@
+import { useSelector, useDispatch } from "react-redux";
 
 function Feeling() {
+    // Declare dispatch
+    const dispatch = useDispatch();
+
+    const feedbackReducer = useSelector(store => store.artistReducer);
+
+    // User will input value
+    // Value is stored in feedbackReducer
+    // This repeats for every form
     return (
         <>
             <h1>How are you feeling today?</h1>
