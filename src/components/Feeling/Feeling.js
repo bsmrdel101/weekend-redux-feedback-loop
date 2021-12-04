d9212d90b6f9b0cc8933395914580c610219b3ef
Bennett Smrdel <smrdelb@gmail.com>
2021-12-04 09:17:29 -0600

    initialized project, and set up skeleton for the first page of the form

---
+++ src/components/Feeling/Feeling.js
@@ -0,0 +1,12 @@
+
+function Feeling() {
+    return (
+        <>
+            <h1>How are you feeling today?</h1>
+            <input type="number"></input>
+            <button>Next</button>
+        </>
+    );
+}
+
+export default Feeling;
