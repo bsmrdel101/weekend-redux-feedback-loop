e71438ad241e203d55ce84f7aed0c76caabf07f0
Bennett Smrdel <smrdelb@gmail.com>
2021-12-05 13:33:11 -0600

    created admin table

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,33 @@
+import { useSelector, useDispatch } from "react-redux";
 
 function Admin() {
+    const feedbackResults = useSelector((store) => store.feedbackReducer);
 
+    // Declare dispatch and history
+    const dispatch = useDispatch();
+    const history = useHistory();
+
+    return (
+        <>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Feeling</th>
+                        <th>Understanding</th>
+                        <th>Support</th>
+                        <th>Comments</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>{feedbackResults[0]}</td>
+                    </tr>
+                </tbody>
+                <tfoot />
+            </table>
+        </>
+    );
 }
 
 export default Admin;
